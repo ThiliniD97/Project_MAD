c895d4a72be21a1886264b072a405ec551c24ee2
ThiliniD97 <thilini.dureksha@yahoo.com>
2019-08-09 15:47:52 +0530

    add Rating bar and complete suggestion

--- app/src/main/java/com/example/mad_project/Suggestions_page.java
+++ app/src/main/java/com/example/mad_project/Suggestions_page.java
@@ -3,12 +3,58 @@ package com.example.mad_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Suggestions_page extends AppCompatActivity {
+
+
+    private static Button buttonS;
+    private static TextView textViewS;
+    private static RatingBar ratingBars;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suggestions_page);
+
+        RatingBarWorking();
+        onButtonClickListener();
     }
+
+    public  void RatingBarWorking(){
+
+        ratingBars=(RatingBar)findViewById(R.id.ratingBar);
+        textViewS=findViewById(R.id.text1);
+
+
+        ratingBars.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float T, boolean b) {
+
+                textViewS.setText(String.valueOf(T));
+            }
+        });
+
+    }
+
+    public void onButtonClickListener(){
+
+        ratingBars=(RatingBar)findViewById(R.id.ratingBar);
+        buttonS=(Button)findViewById(R.id.RsubmitBtn);
+
+        buttonS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Suggestions_page.this,String.valueOf(ratingBars.getRating()),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
 }
