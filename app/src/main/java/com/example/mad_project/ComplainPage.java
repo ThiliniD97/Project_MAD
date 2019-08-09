f1c8a788523da08de4344cfa3b7f04bee28d0eca
ThiliniD97 <thilini.dureksha@yahoo.com>
2019-08-09 18:09:49 +0530

    Done Intent parts of the Complains.

--- app/src/main/java/com/example/mad_project/ComplainPage.java
+++ app/src/main/java/com/example/mad_project/ComplainPage.java
@@ -2,13 +2,48 @@ package com.example.mad_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ComplainPage extends AppCompatActivity {
+
+    private Button button;
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complain_page);
+
+        button= (Button) findViewById(R.id.complain_submit);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                viewComplainsPage();
+            }
+        });
+
+        button1=(Button) findViewById(R.id.cancel2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ViewFeedBackHomeAgain();
+            }
+        });
+
+    }
+
+    public  void viewComplainsPage(){
+        Intent intent = new Intent(this,View_Complains_Activity.class);
+        startActivity(intent);
+    }
+
+    public void ViewFeedBackHomeAgain(){
+        Intent intent = new Intent(this,FeedBackHomeActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/mad_project/View_Complains_Activity.java
@@ -0,0 +1,14 @@
+package com.example.mad_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class View_Complains_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view__complains_);
+    }
+}
